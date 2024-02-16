2185d67d2de1df891a54fcabb6990501bd5192da
Amrutha Vinjamuri <134827744+amrutha1320@users.noreply.github.com>
2024-02-16 22:02:50 +0530

    Add files via upload

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,24 @@
+
+
+
+
+const Header = () => {
+
+    return (
+        <header>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <h2>Add your tasks and make it happen .....</h2>
+            <br/>
+            <br/>
+          
+          
+
+        </header>
+    )
+}
+
+export default Header;
